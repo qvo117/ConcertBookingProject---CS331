2c32f66d34ed5b430df1bea339475b234a1cb006
Dannycom <58293876+Dannycom@users.noreply.github.com>
2021-05-10 06:52:14 +1200

    FINISHED COMMENTS
    all the comments are done!

--- concert-service/src/main/java/asg/concert/service/services/ConcertResource.java
+++ concert-service/src/main/java/asg/concert/service/services/ConcertResource.java
@@ -204,16 +204,18 @@ public class ConcertResource {
 		}
 	}
 
+	//Retrieves all seats by date
 	@GET
 	@Path("seats/{date}")
 	public Response retrieveSeats(@PathParam("date") String dateString, @QueryParam("status") BookingStatus status,
 								  @CookieParam("clientId") Cookie clientId) {
 		try {
 			em.getTransaction().begin();
-			TypedQuery<Seat> seatQuery = em.createQuery("select s from Seat s where s.date = '" + dateString + "'", Seat.class).setLockMode(LockModeType.OPTIMISTIC);
+			TypedQuery<Seat> seatQuery = em.createQuery("select s from Seat s where s.date = '" + dateString + "'", Seat.class).setLockMode(LockModeType.OPTIMISTIC); //seat query by date
 			List<Seat> seats = seatQuery.getResultList();
 			em.getTransaction().commit();
 			List<SeatDTO> dtos = new ArrayList<SeatDTO>();
+			//Check for Booking status and seat availiblity and adds it to the seatDTO if the requirements fit
 			for(Seat seat : seats) {
 				if(status == BookingStatus.Booked && seat.getIsBooked()){
 					dtos.add(SeatMapper.toDto(seat));
@@ -233,9 +235,11 @@ public class ConcertResource {
 		}
 	}
 
+	//Makes a booking
 	@POST
 	@Path("bookings")
 	public Response makeBooking(BookingRequestDTO dto, @CookieParam("auth") Cookie clientId) {
+		//checks if client is logged in
 		if(clientId == null)
 			return Response
 					.status(Response.Status.UNAUTHORIZED)
@@ -244,42 +248,42 @@ public class ConcertResource {
 		try {
 			em.getTransaction().begin();
 			Booking booking;
-			Concert concert = em.find(Concert.class, dto.getConcertId());
-			if(concert == null || !concert.getDates().contains(dto.getDate()))
-				return Response
+			Concert concert = em.find(Concert.class, dto.getConcertId());	//finds concert that matches the bookingRequestDTO
+			if(concert == null || !concert.getDates().contains(dto.getDate())) //Check if there is a concert on a certain date
+				return Response												//reponse if concert does not exsist
 						.status(Response.Status.BAD_REQUEST)
 						.cookie(makeCookie(clientId))
 						.build();
 			User user;
 			try {
-				user = em.find(User.class, Long.parseLong(clientId.getValue()));
+				user = em.find(User.class, Long.parseLong(clientId.getValue()));	//find user based on specified id
 				List<Seat> bookingSeats = new ArrayList<Seat>();
 				for(String label : dto.getSeatLabels()) {
 					TypedQuery<Seat> seatQuery = em.createQuery("select s from Seat s where s.date = '" + dto.getDate().toString()
-																+ "' and s.label = '" + label + "'", Seat.class).setLockMode(LockModeType.OPTIMISTIC);
+																+ "' and s.label = '" + label + "'", Seat.class).setLockMode(LockModeType.OPTIMISTIC); //queries seats where the date and label match
 					Seat seat = seatQuery.getSingleResult();
 					if(seat.getIsBooked()) {
-						return Response
+						return Response										//response if the seat is already booked
 								.status(Response.Status.FORBIDDEN)
 								.cookie(makeCookie(clientId))
 								.build();
 					}
-					seat.setIsBooked(true);
+					seat.setIsBooked(true); //if not booked
 					bookingSeats.add(seat);
 					em.merge(seat);
 				}
-				booking = new Booking(dto.getConcertId(), dto.getDate(), bookingSeats);
+				booking = new Booking(dto.getConcertId(), dto.getDate(), bookingSeats); //bookingDTO based on the the newly booked seats
 				booking.setUser(user);
 				em.persist(booking);
 			}
-			catch(NumberFormatException e) {
+			catch(NumberFormatException e) {						//when the user login fails
 				return Response
 						.status(Response.Status.UNAUTHORIZED)
 						.cookie(makeCookie(clientId))
 						.build();
 			}
 			em.getTransaction().commit();
-			return Response.created(URI
+			return Response.created(URI											//response for successful booking of seats
 					.create("/concert-service/bookings/" + booking.getId()))
 					.status(Response.Status.CREATED)
 					.cookie(makeCookie(clientId))
@@ -290,10 +294,11 @@ public class ConcertResource {
 		}
 	}
 
+	//Retrieve all the bookings based on user id
 	@GET
 	@Path("bookings")
 	public Response retrieveAllBookings(@CookieParam("auth") Cookie clientId) {
-		if(clientId == null)
+		if(clientId == null)									//checks login
 			return Response
 					.status(Response.Status.UNAUTHORIZED)
 					.cookie(makeCookie(clientId))
@@ -302,50 +307,17 @@ public class ConcertResource {
 			em.getTransaction().begin();
 			User user;
 			try {
-				user = em.find(User.class, Long.parseLong(clientId.getValue()));
+				user = em.find(User.class, Long.parseLong(clientId.getValue()));	//gets user based on id
 				TypedQuery<Booking> bookingsQuery = em.createQuery("select b from Booking b where b.user = :user", 
-															 Booking.class).setParameter("user", user);
+															 Booking.class).setParameter("user", user); 		//Queries all the bookings related to the user
 				List<Booking> bookings = bookingsQuery.getResultList();
 				List<BookingDTO> dtos = new ArrayList<BookingDTO>();
-				for(Booking booking : bookings)
+				for(Booking booking : bookings)							//all the booking list to bookingDTO list
 					dtos.add(BookingMapper.toDto(booking));
 				GenericEntity<List<BookingDTO>> entity = new GenericEntity<List<BookingDTO>>(dtos) {};
-				return Response.ok(entity).cookie(makeCookie(clientId)).build();
+				return Response.ok(entity).cookie(makeCookie(clientId)).build();	//Returns all the bookings
 			}
-			catch(NumberFormatException e) {
-				return Response
-						.status(Response.Status.UNAUTHORIZED)
-						.cookie(makeCookie(clientId))
-						.build();
-			}
-		}
-		finally {
-			em.close();
-		}
-	}
-	
-	@GET
-	@Path("bookings/{id}")
-	public Response retrieveBooking(@PathParam("id") Long id, @CookieParam("auth") Cookie clientId) {
-		if(clientId == null)
-			return Response
-					.status(Response.Status.UNAUTHORIZED)
-					.cookie(makeCookie(clientId))
-					.build();
-		try {
-			em.getTransaction().begin();
-			User user;
-			try {
-				user = em.find(User.class, Long.parseLong(clientId.getValue()));
-				Booking booking = em.find(Booking.class, id);
-				if(!booking.getUser().equals(user))
-					return Response
-							.status(Response.Status.FORBIDDEN)
-							.cookie(makeCookie(clientId))
-							.build();
-				return Response.ok(BookingMapper.toDto(booking)).cookie(makeCookie(clientId)).build();
-			}
-			catch(NumberFormatException e) {
+			catch(NumberFormatException e) {							//response for failed login
 				return Response
 						.status(Response.Status.UNAUTHORIZED)
 						.cookie(makeCookie(clientId))
@@ -357,11 +329,47 @@ public class ConcertResource {
 		}
 	}
 
+	//Retrieves booking based on booking id
+	@GET
+	@Path("bookings/{id}")
+	public Response retrieveBooking(@PathParam("id") Long id, @CookieParam("auth") Cookie clientId) {
+		if(clientId == null)		//checking login
+			return Response
+					.status(Response.Status.UNAUTHORIZED)
+					.cookie(makeCookie(clientId))
+					.build();
+		try {
+			em.getTransaction().begin();
+			User user;
+			try {
+				user = em.find(User.class, Long.parseLong(clientId.getValue()));
+				Booking booking = em.find(Booking.class, id);	//finds booking that matches the given booking id
+				if(!booking.getUser().equals(user))
+					return Response										//if the user that booked it doesn't match the booking's user it return this
+							.status(Response.Status.FORBIDDEN)
+							.cookie(makeCookie(clientId))
+							.build();
+				//response if the booking with the given booking id matches with the user of that booking
+				return Response.ok(BookingMapper.toDto(booking)).cookie(makeCookie(clientId)).build();
+			}
+			catch(NumberFormatException e) {		//failed login
+				return Response
+						.status(Response.Status.UNAUTHORIZED)
+						.cookie(makeCookie(clientId))
+						.build();
+			}
+		}
+		finally {
+			em.close();
+		}
+	}
+
+	//Notifies that if the percentage if available seats is below a certain amount
 	@GET
 	@Path("subscribe/concertInfo")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response makeSubscription(@CookieParam("auth") Cookie clientId){
-		if(clientId == null)
+	public Response subscriptionNotify(@CookieParam("auth") Cookie clientId){
+		if(clientId == null)					//checks login
 			return Response
 					.status(Response.Status.UNAUTHORIZED)
 					.cookie(makeCookie(clientId))
@@ -370,17 +378,19 @@ public class ConcertResource {
 			em.getTransaction().begin();
 			User user = em.find(User.class, Long.parseLong(clientId.getValue()));
 			synchronized (subs){
-				for (int i = 0; i < subs.size(); i++) {
+				for (int i = 0; i < subs.size(); i++) {							//loops through the subDTO and subs lists
 					ConcertInfoSubscriptionDTO subdto = subDtos.get(i);
+					//Query for seats matching date and if the seat is booked
 					List<Seat> seats = em.createQuery("select s from seats s where s.date = '" +
 							subdto.getDate().toString() + "' and s.isBooked = :isBooked", Seat.class).setLockMode(LockModeType.OPTIMISTIC).setParameter("isBooked", true).getResultList();
-					if(seats.isEmpty() == true || em.find(Concert.class, subdto.getConcertId()) == null)
+					if(seats.isEmpty() == true || em.find(Concert.class, subdto.getConcertId()) == null) //If no seats exist for that day or if the concert id refers to a concert that does not exsist
 						return Response
 								.status(Response.Status.BAD_REQUEST)
 								.cookie(makeCookie(clientId))
 								.build();
+					//Otherwise it checks if seats left percentage is greater than or equal to the subscription percentage
 					if((seats.size()/120)*100 >= subdto.getPercentageBooked()){
-						ConcertInfoNotificationDTO dto = new ConcertInfoNotificationDTO(120 - seats.size());
+						ConcertInfoNotificationDTO dto = new ConcertInfoNotificationDTO(120 - seats.size()); //seats remaining
 						subs.get(i).resume(dto);
 					}
 				}
@@ -389,7 +399,7 @@ public class ConcertResource {
 			em.getTransaction().commit();
 			return Response.ok().cookie(makeCookie(clientId)).build();
 		}
-		catch(NumberFormatException e) {
+		catch(NumberFormatException e) {		//response failed login
 			return Response
 					.status(Response.Status.UNAUTHORIZED)
 					.cookie(makeCookie(clientId))
@@ -400,11 +410,11 @@ public class ConcertResource {
 		}
 	}
 
-
+	//User subscribes
 	@POST
 	@Path("subscribe/concertInfo")
-	public void subscriptionNotify(final @Suspended AsyncResponse sub, @CookieParam("auth") Cookie clientId, ConcertInfoSubscriptionDTO dto) {
-		if(clientId == null)
+	public void makeSubscription(final @Suspended AsyncResponse sub, @CookieParam("auth") Cookie clientId, ConcertInfoSubscriptionDTO dto) {
+		if(clientId == null)		//checks login
 			throw new WebApplicationException(Response
 					.status(Response.Status.UNAUTHORIZED)
 					.cookie(makeCookie(clientId))
@@ -412,13 +422,13 @@ public class ConcertResource {
 		User user;
 		try {
 			em.getTransaction().begin();
-			user = em.find(User.class, Long.parseLong(clientId.getValue()));
-			subDtos.add(dto);
-			subs.add(sub);
+			user = em.find(User.class, Long.parseLong(clientId.getValue())); //finds user based on clientId
+			subDtos.add(dto); 										//Adds subInfoDTO to subDTO list
+			subs.add(sub);											//Adds sub asyncResponse to a sub responder list
 			em.merge(user);
 			em.getTransaction().commit();
 		}
-		catch(NumberFormatException e) {
+		catch(NumberFormatException e) {		//failed login
 			throw new WebApplicationException(Response
 					.status(Response.Status.UNAUTHORIZED)
 					.cookie(makeCookie(clientId))
@@ -429,6 +439,7 @@ public class ConcertResource {
 		}
 	}
 
+	//Makes cookies
 	private NewCookie makeCookie(Cookie clientId) {
         NewCookie newCookie = null;
 
